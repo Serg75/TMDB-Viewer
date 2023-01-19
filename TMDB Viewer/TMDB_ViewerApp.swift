bd7702f76d36d4a5f56274a766a278c3b087cb57
Sergey Slobodenyuk <sergn75@gmail.com>
2023-01-19 10:37:58 +0100

    Initial Commit

---
+++ TMDB Viewer/TMDB_ViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMDB_ViewerApp.swift
+//  TMDB Viewer
+//
+//  Created by Sergey Slobodenyuk on 2023-01-19.
+//
+
+import SwiftUI
+
+@main
+struct TMDB_ViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
